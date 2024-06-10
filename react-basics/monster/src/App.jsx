8c4a773714df39ab0b148144e60138f43079b7b6
jstgrowup <deysubham999@gmail.com>
2024-06-10 10:55:56 +0530

    class to functional components

--- react-basics/monster/src/App.jsx
+++ react-basics/monster/src/App.jsx
@@ -1,43 +1,79 @@
-import { Component } from "react";
+import { Component, useState, useEffect } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
-  onSearchChange = (e) => {
-    const searchField = e.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter((monster) => {
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
-    return (
-      <>
-        <SearchBox onChangeHandler={this.onSearchChange} />
-        <CardList monsters={filteredMonsters} />
-      </>
-    );
-  }
-}
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+
+      <SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//   }
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { monsters: users };
+//         })
+//       );
+//   }
+//   onSearchChange = (e) => {
+//     const searchField = e.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   };
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+//     return (
+//       <>
+//         <SearchBox onChangeHandler={this.onSearchChange} />
+//         <CardList monsters={filteredMonsters} />
+//       </>
+//     );
+//   }
+// }
 
 export default App;
--- react-basics/monster/src/components/card-list/card-list.component.jsx
+++ react-basics/monster/src/components/card-list/card-list.component.jsx
@@ -1,27 +1,50 @@
 import React, { Component } from "react";
 
-class CardList extends Component {
-  render() {
-    console.log("render from cardlist");
-    const { monsters } = this.props;
-    return (
-      <div>
-        {monsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <div className="card-container">
-                <img
-                  src={`https://robohash.org/${monster.id}?set=set2`}
-                  alt={`monster ${monster.name}`}
-                />{" "}
-                <h2>{monster?.name}</h2>
-                <p>{monster?.email}</p>
-              </div>
+const CardList = ({ monsters }) => {
+  console.log("monsters:", monsters);
+  return (
+    <div>
+      {monsters?.map((monster) => {
+        return (
+          <div key={monster.id}>
+            <div className="card-container">
+              <img
+                src={`https://robohash.org/${monster.id}?set=set2`}
+                alt={`monster ${monster.name}`}
+              />{" "}
+              <h2>{monster?.name}</h2>
+              <p>{monster?.email}</p>
             </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default CardList;
+// class CardList extends Component {
+//   render() {
+//     console.log("render from cardlist");
+//     const { monsters } = this.props;
+//     return (
+//       <div>
+//         {monsters.map((monster) => {
+//           return (
+//             <div key={monster.id}>
+//               <div className="card-container">
+//                 <img
+//                   src={`https://robohash.org/${monster.id}?set=set2`}
+//                   alt={`monster ${monster.name}`}
+//                 />{" "}
+//                 <h2>{monster?.name}</h2>
+//                 <p>{monster?.email}</p>
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
+// export default CardList;
