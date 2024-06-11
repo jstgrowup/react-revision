8c1c7edea13f254164c8f1a3c1979040249340ad
jstgrowup <deysubham999@gmail.com>
2024-06-11 23:26:14 +0530

    capstone project initialization

--- react-basics/monster/src/main.jsx
+++ react-basics/monster/src/main.jsx
@@ -3,6 +3,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 const container = document.getElementById("root");
-
 const root = createRoot(container);
 root.render(<App />);
