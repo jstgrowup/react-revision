dddb25cabf0008abed97fb8c907a2f4b628faa7c
jstgrowup <deysubham999@gmail.com>
2024-06-21 10:39:45 +0530

    fixes in the form input component

--- capstone-project/src/components/form-input/form-input.component.jsx
+++ capstone-project/src/components/form-input/form-input.component.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-
+import "./form-input.component.scss";
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <>
+    <div className="group">
       {" "}
-      <label htmlFor="">{label}</label>
-      <input {...otherProps} />
-    </>
+      <input className="form-input" {...otherProps} />
+      {label && (
+        <label
+          className={`${
+            otherProps.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      )}
+    </div>
   );
 };
 
--- capstone-project/src/components/sign-up-form/sign-up-form.component.jsx
+++ capstone-project/src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
+
 const defaultFormFields = {
   displayName: "",
   email: "",
