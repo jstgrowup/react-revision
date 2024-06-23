8c69f64d9f489ee1c4932163c352757f55694848
jstgrowup <deysubham999@gmail.com>
2024-06-23 13:35:00 +0530

    implementing the context

--- capstone-project/src/main.jsx
+++ capstone-project/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./context/user.context.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
