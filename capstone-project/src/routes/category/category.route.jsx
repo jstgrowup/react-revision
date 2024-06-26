88450781d75e93a4a496bbaf95fecd13fd798392
jstgrowup <deysubham999@gmail.com>
2024-06-27 00:20:51 +0530

    categories preview

---
+++ capstone-project/src/routes/category/category.route.jsx
@@ -0,0 +1,23 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { CategoriesContext } from "../../context/categories.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <div className="category-container">
+      {products?.map((product) => {
+        return <ProductCard key={product.id} product={product} />;
+      })}
+    </div>
+  );
+};
+
+export default Category;
--- capstone-project/src/routes/shop/shop.component.jsx
+++ capstone-project/src/routes/shop/shop.component.jsx
@@ -1,21 +1,13 @@
-import React, { Fragment, useContext } from "react";
-
-import { CategoriesContext } from "../../context/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
-import "./shop.styles.scss";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.route";
+import Category from "../category/category.route";
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />;
+      <Route path=":category" element={<Category />} />;
+    </Routes>
   );
 };
 
